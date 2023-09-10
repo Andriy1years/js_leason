4374b7f1f722f6dad17e51762945f06f547111a9
Andriy1years <andricho1905@gmail.com>
2023-09-10 23:38:01 +0300

    cvx

--- practic_2/js.js
+++ practic_2/js.js
@@ -1,4 +1,16 @@
-const STRANGE_STRING = "";
-console.log(STRANGE_STRING);
-return(STRANGE_STRING.toLowerCase());
-
+function circleArea(radius) {
+    console.log(radius)
+    const p = 3.14;
+    let radius2 = radius * 5;
+  
+    if(radius > 0){
+      return(p * radius);
+    }
+    else if(radius > 30) {
+      return(p * radius2);
+    }
+    else if (radius => 0) {
+      throw new Error("Error")
+    }
+    return 0;
+  }
